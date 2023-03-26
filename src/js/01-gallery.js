a03b914896867405c1b0e5446c2784d839ad980d
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-03-26 22:43:25 +0300

    update
    update gallery,css for gallery.

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector('.gallery');
+
+// console.log(galleryListEl);
+
+const imageForGalleryList = galleryItems
+  .map(
+    item => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+  )
+  .join(' ');
+
+galleryListEl.insertAdjacentHTML('beforeend', imageForGalleryList);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
