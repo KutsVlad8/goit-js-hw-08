d791f992d48d913c2a1cf0b0aecfc097e5909eed
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-03-30 00:03:23 +0300

    update task-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
+
 const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', onPlay);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+function onPlay(data) {
+  localStorage.setItem(CURRENT_TIME, data.seconds);
+}
+
+player.setCurrentTime().then(function () {});
